8a231af0cbbf87e73d21ab9f09f270d8b6ac1add
Abhi Dutta <abhid94@gmail.com>
2017-12-11 20:45:08 +1100

    Excel upload currently working and saving to uploads folder

--- app.js
+++ app.js
@@ -3,31 +3,94 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
+//for file upload
+var bodyParser = require('body-parser');
+var multer = require('multer');
+
+//to convert excel files to JSON
+var xlstojson = require("xls-to-json-lc");
+var xlsxtojson = require("xlsx-to-json-lc");
+
+app.use(bodyParser.json());
 //set the view engine to ejs
 app.set('view engine', 'ejs');
 
 //static files
 app.use(express.static('./public'));
 
+
+//multers disk storage settings
+
+var storage = multer.diskStorage({ //multers disk storage settings
+        destination: function (req, file, cb) {
+            cb(null, './uploads/')
+        },
+        filename: function (req, file, cb) {
+            var datetimestamp = Date.now();
+            cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1])
+        }
+    });
+
+
+//multer settings
+var upload = multer({ //multer settings
+                    storage: storage,
+                    fileFilter : function(req, file, callback) { //file filter
+                        if (['xls', 'xlsx'].indexOf(file.originalname.split('.')[file.originalname.split('.').length-1]) === -1) {
+                            return callback(new Error('Wrong extension type'));
+                        }
+                        callback(null, true);
+                    }
+                }).single('file');
+
+    /** API path that will upload the files */
+    app.post('/upload', function(req, res) {
+        var exceltojson;
+        upload(req,res,function(err){
+            if(err){
+                 res.json({error_code:1,err_desc:err});
+                 return;
+            }
+            /** Multer gives us file info in req.file object */
+            if(!req.file){
+                res.json({error_code:1,err_desc:"No file passed"});
+                return;
+            }
+            /** Check the extension of the incoming file and
+             *  use the appropriate module
+             */
+            if(req.file.originalname.split('.')[req.file.originalname.split('.').length-1] === 'xlsx'){
+                exceltojson = xlsxtojson;
+            } else {
+                exceltojson = xlstojson;
+            }
+            console.log(req.file.path);
+            try {
+                exceltojson({
+                    input: req.file.path,
+                    output: null, //since we don't need output.json
+                    lowerCaseHeaders:true
+                }, function(err,result){
+                    if(err) {
+                        return res.json({error_code:1,err_desc:err, data: null});
+                    }
+                    res.json({data: result});
+                    console.log(result);
+                });
+            } catch (e){
+                res.json({error_code:1,err_desc:"Corupted excel file"});
+            }
+        })
+
+    });
+
+
 // use res.render to load up an ejs view file
 // index page
 app.get('/', function(req, res) {
-  var drinks = [
-      { name: 'Bloody Mary', drunkness: 3 },
-      { name: 'Martini', drunkness: 5 },
-      { name: 'Scotch', drunkness: 10 }
-  ];
-  var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+  res.sendFile(__dirname + "/index.ejs");
+  res.render('pages/index');
 
-  res.render('pages/index', {
-      drinks: drinks,
-      tagline: tagline
-  });
-});
-
-// about page
-app.get('/about', function(req, res) {
-    res.render('pages/about');
 });
 
 //listen to port
