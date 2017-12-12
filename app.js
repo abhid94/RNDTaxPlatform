e1661e1422352b5e816f67cd5218c828d4d31465
Abhi Dutta <abhid94@gmail.com>
2017-12-13 00:48:57 +1100

    Changed code to not hard set a port to deploy to Heroku

--- app.js
+++ app.js
@@ -17,5 +17,9 @@ app.use(express.static('./public'));
 indexController(app);
 
 //listen to port
-app.listen(3000);
-console.log('Listenting to port 3000');
+//app.listen(3000);
+//console.log('Listenting to port 3000');
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
